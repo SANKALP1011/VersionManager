6a91414b3203aa55c8c66ef332e367b5de183dba
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-07-08 17:18:43 +0530

    working on the schema for the database

---
+++ Model/User.Model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  GithubId: {
+    type: String,
+    default: "",
+  },
+  GithubUserName: {
+    type: String,
+    default: "",
+  },
+  GithubDisplayName: {
+    type: String,
+    default: "",
+  },
+  GitHubAvatar: {
+    type: String,
+    default: "",
+  },
+  Company: {
+    type: String,
+    default: "",
+  },
+  GitHubProtfolioa: {
+    type: String,
+    default: "",
+  },
+  Location: {
+    type: String,
+    default: "",
+  },
+  GitHubBio: {
+    type: String,
+    default: "",
+  },
+  PublicRepos: {
+    type: Number,
+    default: 0,
+  },
+  GiHubFollowres: {
+    type: Number,
+    default: 0,
+  },
+  GitHubFollowing: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("user", UserSchema);
