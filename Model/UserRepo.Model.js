d3563c6c8a4c49b5222946bbbdb2873be5b8b367
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-07-14 01:51:33 +0530

    designned an efficient schema to save the user repo data , would be used later for analysis

--- Model/UserRepo.Model.js
+++ Model/UserRepo.Model.js
@@ -3,8 +3,93 @@ const mongoose = require("mongoose");
 const UserRepoSchema = new mongoose.Schema({
   UserId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+    ref: "User",
     unique: true,
   },
-  
+  repositories: [
+    {
+      name: {
+        type: String,
+        default: "",
+      },
+      private: {
+        type: Boolean,
+        default: false,
+      },
+      repoUrl: {
+        type: String,
+        default: "",
+      },
+      description: {
+        type: String,
+        default: "",
+      },
+      dateOfCreation: {
+        type: Date,
+        default: Date.now,
+      },
+      repoStarsCount: {
+        type: Number,
+        default: 0,
+      },
+      repoWatchersCount: {
+        type: Number,
+        default: 0,
+      },
+      buildLanguage: {
+        type: String,
+        default: "",
+      },
+      repoForkCounts: {
+        type: Number,
+        default: 0,
+      },
+      languagesUsedUrl: {
+        type: String,
+        default: "",
+      },
+      openIssuesCount: {
+        type: Number,
+        default: 0,
+      },
+      repo_topics: {
+        type: [String],
+        default: [],
+      },
+      line_of_count: {
+        type: map,
+        of: Number,
+      },
+      branches: [
+        {
+          name: {
+            type: String,
+            default: "",
+          },
+          url: {
+            type: String,
+            default: "",
+          },
+        },
+      ],
+      commit_history: [
+        {
+          author_Namae: {
+            type: String,
+            default: "",
+          },
+          author_commit_message: {
+            type: String,
+            default: "",
+          },
+        },
+      ],
+      repo_readme_content: {
+        type: String,
+        default: "",
+      },
+    },
+  ],
 });
+
+module.exports = mongoose.model("UserRepo", UserRepoSchema);
