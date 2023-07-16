75f5cb66c668b4c7c42b161262c56fe579dbe115
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-07-16 20:45:18 +0530

    working on the github rest api error

--- Github Service/repo.service.js
+++ Github Service/repo.service.js
@@ -1,24 +1,28 @@
 const { Octokit } = require("@octokit/rest");
 const axios = require("axios");
+const { request } = require("@octokit/request");
+const tok = "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";
 
 const octokit = new Octokit({
-  auth: "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",
-  request: {
-    fetch: axios,
-  },
+  auth: tok,
 });
 
 module.exports = {
   getUserRepo: async (username) => {
     console.log(username);
     try {
-      const { data } = await octokit.rest.repos.listForUser({
-        username: username,
+      const result = await request(`GET /users/${username}/repos`, {
+        headers: {
+          Accept: "application/vnd.github.v3+json",
+          authorization: `bearer ${tok}`,
+        },
       });
-      console.log(data);
-      return data;
+
+      console.log(result);
+      return result;
     } catch (err) {
-      throw new Error(err.message);
+      console.error(err);
+      return null;
     }
   },
 };
