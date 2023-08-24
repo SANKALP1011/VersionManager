356fc65ee1a851babe298f5e72cfbaf45ea87a85
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-08-24 21:39:16 +0530

    organisisng files for the project

--- Controller/repoStatisticalAnalysis.controller.js
+++
@@ -1,9 +0,0 @@
-const User = require("../Model/User.model");
-const Repository = require("../Model/UserRepo.Model");
-const PullRequest = require("../Model/UserPullRequest.model");
-
-module.exports = {
-  // working
-};
-
-// DECIDE ON MORE FACTORS AND FIND THE WAY HOW TO GET ALL THESE THINGS FOR ANALYSIS IE WHEN USER CLICKS ON REPO ALL THESE ANALYSIS SHOULD BE AVAILAIBLE.
--- Parallel/multipleApiCall.parallel.js
+++
@@ -1,5 +0,0 @@
-const axios = require("axios");
-
-module.exports = {
-  // here , we would make parallel api call to our multiple endpoints by passing the nessory information to the query
-};
